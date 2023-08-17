49d4764c251e6844af13ed43edd2176488fb771b
ErnestoGranja <129196438+ErnestoGranja@users.noreply.github.com>
2023-08-16 22:53:25 -0300

    Update Principal.js

--- Principal.js
+++ Principal.js
@@ -1,34 +1,22 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore, doc } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDbwR2ttx2iGWiUe9QsC_PvYC2Uth11tHo",
-  authDomain: "projectusch.firebaseapp.com",
-  projectId: "projectusch",
-  storageBucket: "projectusch.appspot.com",
-  messagingSenderId: "827966120288",
-  appId: "1:827966120288:web:cf92e570dcb020c2133e05",
-  measurementId: "G-QPWX2MMSR3"
+  // Tu configuración de Firebase aquí
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
-
 function consultarColeccion() {
-  const db = firebase.firestore();
-  const docRef = db.collection('prueba1').doc(Administrar);
+  const db = getFirestore();
+  const docRef = doc(db, 'prueba1', 'ID_DEL_DOCUMENTO_A_CONSULTAR'); // Reemplaza 'ID_DEL_DOCUMENTO_A_CONSULTAR' con el ID real
 
-docRef.get().then((doc) => {
-    if (doc.exists) {
-      const data = doc.data(); // Obtener todos los campos del documento
-      const nombre = data.nombre; // Acceder al campo "nombre"
+  docRef.get().then((doc) => {
+    if (doc.exists()) {
+      const data = doc.data();
+      const nombre = data.nombre;
       console.log('Nombre:', nombre);
     } else {
       console.log('Documento no encontrado.');
@@ -36,9 +24,8 @@ docRef.get().then((doc) => {
   }).catch((error) => {
     console.log('Error obteniendo el documento:', error);
   });
-};
-
+}
 
 window.onload = function() {
-  obtenerCampoNombre('ID_DEL_DOCUMENTO_A_CONSULTAR');
+  consultarColeccion();
 };
